630ba7e03c30a5bd1de96dfa3e583d392b362e35
ripper <libor.ripper@tipsport.cz>
2025-04-30 09:10:35 +0200

    navigate to easyFe repo

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,14 +3,15 @@ import { click, fill, navigate, launchApp, wait } from '../src/DSL';
 const platform = process.argv[2];
 
 if (platform === 'web') {
-    navigate('https://github.com/search?q=razzyripper&type=repositories');
+    navigate('https://github.com/razzyrip');
   } else if (platform === 'native') {
     launchApp('cz.example-your.app'); // Bundle ID tvé android appky
   }
   wait(3000) // čeká 3 sekundy 
 platform == 'native' ? click("NOT NOW") : wait(3000)  
-wait(2000)
-click("razzyripper/razzyripper");
+navigate('https://github.com/razzyrip?tab=repositories')
+wait(3000)
+click("easyFE");
 wait(3000)
 //npm run test:web
 //npm run test:native
